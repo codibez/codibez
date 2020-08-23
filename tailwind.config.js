1b491c4211a5c5d65d4fc923b1a92daefe0de906
Codi Bezouka-Smith <codifuadbezoukasmith@gmail.com>
2020-08-23 12:42:13 -0400

    Add tailwindCSS & core node modules

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: '2.5rem',
+      },
+      textColor: {
+        'gray': '#B3B3B3',
+        'red': '#D36363',
+        'black-3/4': '#1A1A1A',
+      },
+      backgroundColor: {
+        'red-450': '#D36363',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
