069294e35a9e0231572b9ae734c2e9ae62687bff
Codi Bezouka-Smith <hello@codibez.dev>
2020-08-24 14:00:50 -0400

    Added header

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,7 @@ module.exports = {
   theme: {
     extend: {
       container: {
-        center: true,
-        padding: '2.5rem',
+        center: false
       },
       textColor: {
         'gray': '#B3B3B3',
@@ -15,7 +14,7 @@ module.exports = {
         'black-3/4': '#1A1A1A',
       },
       backgroundColor: {
-        'red-450': '#D36363',
+        'red': '#D36363',
       }
     },
   },
